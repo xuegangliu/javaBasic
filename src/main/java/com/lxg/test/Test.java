02f724a002e3e4462c4550059e734ed339a74722
Max <lxghoney@163.com>
2017-09-11 16:51:37 +0800

    update README

---
+++ src/main/java/com/lxg/test/Test.java
@@ -0,0 +1,12 @@
+package com.lxg.test;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] a={1,5,6,5,55,9,4,5,6,66};
+        int b[]={1,2,3,4,5};
+        System.out.println(a.length);
+        System.out.println(b.length);
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+    }
+}

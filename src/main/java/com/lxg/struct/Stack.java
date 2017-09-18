cacad5d65dd300b8c887fc988f49711503678371
Max <lxghoney@163.com>
2017-09-18 23:26:48 +0800

    add java stack struct demo 单词逆序

--- src/main/java/com/lxg/struct/Stack.java
+++ src/main/java/com/lxg/struct/Stack.java
@@ -6,22 +6,22 @@ import java.util.Scanner;
  * 栈是先进后出的数据的结构
  */
 public class Stack {
-    private int array[];
+    private char array[];
     private int max;
     private int top;
     public Stack(int max){
         this.max = max;
-        array = new int[max];
+        array = new char[max];
         top = 0;
     }
-    public void push(int value){
+    public void push(char value){
         if(isFull()){
             System.out.println("full,can not insert");
             return;
         }
         array[top++]=value;
     }
-    public int pop(){
+    public char pop(){
         return array[--top];
     }
     public boolean isEmpty(){
@@ -41,13 +41,35 @@ public class Stack {
             System.out.println(pop());
         }
     }
+
+    /**
+     * 单词逆序
+     * @param in
+     * @return
+     */
+    public String reverse(String in){
+        String out="";
+        for (int i = 0; i < in.length(); i++) {
+            char c = in.charAt(i);
+            push(c);
+        }
+        while(!isEmpty()){
+            out+=pop();
+        }
+        return out;
+    }
     public static void main(String[] args) {
-        Stack s = new Stack(5);
-        s.push(1);
-        s.push(3);
-        s.push(5);
-        s.push(5);
-        s.push(5);
-        s.display();//5 5 5 3 1
+        // char array[] 修改为int array[]
+//        Stack s = new Stack(5);
+//        s.push(1);
+//        s.push(3);
+//        s.push(5);
+//        s.push(5);
+//        s.push(5);
+//        s.display();//5 5 5 3 1
+        Scanner s = new Scanner(System.in);
+        String string = s.nextLine();
+        Stack st = new Stack(string.length());
+        System.out.println(st.reverse(string));
     }
 }

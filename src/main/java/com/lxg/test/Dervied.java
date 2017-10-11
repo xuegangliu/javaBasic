a8648dca34254dba32086aa728384b35ff40cffa
Max <lxghoney@163.com>
2017-10-11 21:40:05 +0800

    子类与父类有同样的属性练习

---
+++ src/main/java/com/lxg/test/Dervied.java
@@ -0,0 +1,48 @@
+package com.lxg.test;
+
+public class Dervied extends Base {
+
+    private String name = "dervied";
+
+    public Dervied() {
+        tellName();
+        printName();
+    }
+
+    public void tellName() {
+        System.out.println("Dervied tell name: " + name);
+    }
+
+    public void printName() {
+        System.out.println("Dervied print name: " + name);
+    }
+
+    public static void main(String[] args){
+
+        new Dervied();
+ // 打印如下
+//        Dervied tell name: null
+//        Dervied print name: null
+//        Dervied tell name: dervied
+//        Dervied print name: dervied
+    }
+}
+
+class Base {
+
+    // 私有属性
+    private String name = "base";
+
+    public Base() {
+        tellName();
+        printName();
+    }
+
+    public void tellName() {
+        System.out.println("Base tell name: " + name);
+    }
+
+    public void printName() {
+        System.out.println("Base print name: " + name);
+    }
+}

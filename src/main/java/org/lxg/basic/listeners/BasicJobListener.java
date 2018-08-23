068d604f4c568b95baef527a0200cce33fb4eb39
Max <lxghoney@163.com>
2018-08-23 10:25:22 +0800

    添加quartz 测试

---
+++ src/main/java/org/lxg/basic/listeners/BasicJobListener.java
@@ -0,0 +1,34 @@
+package org.lxg.basic.listeners;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.JobListener;
+
+/**
+ * @author: xuegangliu
+ * @date: 8/22/2018 6:14 PM
+ * @DES:
+ * @version: v1.0
+ */
+public class BasicJobListener implements JobListener {
+    @Override
+    public String getName() {
+        System.out.println("getName");
+        return "BasicJobListener";
+    }
+
+    @Override
+    public void jobToBeExecuted(JobExecutionContext jobExecutionContext) {
+        System.out.println("jobToBeExecuted");
+    }
+
+    @Override
+    public void jobExecutionVetoed(JobExecutionContext jobExecutionContext) {
+        System.out.println("jobExecutionVetoed");
+    }
+
+    @Override
+    public void jobWasExecuted(JobExecutionContext jobExecutionContext, JobExecutionException e) {
+        System.out.println("jobWasExecuted");
+    }
+}

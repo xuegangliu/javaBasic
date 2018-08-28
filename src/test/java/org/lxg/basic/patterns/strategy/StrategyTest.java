25f847c9599a1f348338d057e152702e6d6c3d46
Max <lxghoney@163.com>
2018-08-28 15:42:55 +0800

    add strategy patterns sample test

--- src/test/java/org/lxg/basic/patterns/strategy/StrategyTest.java
+++ src/test/java/org/lxg/basic/patterns/strategy/StrategyTest.java
@@ -1,10 +1,34 @@
 package org.lxg.basic.patterns.strategy;
 
+import org.junit.Test;
+
 /**
- * Created by Administrator on 2017/3/6 0006.
+ * @author: xuegangliu
+ * @date: 8/28/2018 3:40 PM
+ * @DES:
+ * @version: v1.0
  */
-public class Calculator {
-    public static void main(String[] args) {
+public class StrategyTest {
+
+    @Test
+    public void strategyTest(){
+        Context context = new Context();
+
+        context.setCompute(new Add());
+        context.calc(1,2);
+
+        context.setCompute(new Sub());
+        context.calc(3,4);
+
+        context.setCompute(new Mul());
+        context.calc(5,6);
+
+        context.setCompute(new Div());
+        context.calc(7,8);
+    }
+
+    @Test
+    public void computeTest(){
         System.out.println("计算：1 + 1 = " + compute("+", 1, 1));
         System.out.println("计算：1 - 1 = " + compute("-", 1, 1));
         System.out.println("计算：1 * 1 = " + compute("*", 1, 1));

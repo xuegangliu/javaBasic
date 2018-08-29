7b62e3405ee87926e97ea9b8e2996002efc2dd9c
Max <lxghoney@163.com>
2018-08-29 16:34:10 +0800

    add single patterns sample test

---
+++ src/test/java/org/lxg/basic/patterns/single/SingleTest.java
@@ -0,0 +1,17 @@
+package org.lxg.basic.patterns.single;
+
+import org.junit.Test;
+
+/**
+ * @author: xuegangliu
+ * @date: 8/29/2018 4:31 PM
+ * @DES:
+ * @version: v1.0
+ */
+public class SingleTest {
+
+    @Test
+    public void singleTest(){
+        System.out.println(Single_1.getInstance());
+    }
+}

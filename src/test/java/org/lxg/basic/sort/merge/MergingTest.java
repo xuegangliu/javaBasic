e22006281ebc76c8fe4b977fd615233cf2b9f179
Max <lxghoney@163.com>
2018-09-12 11:07:33 +0800

    选择排序添加

--- src/test/java/org/lxg/basic/sort/merge/MergingTest.java
+++ src/test/java/org/lxg/basic/sort/merge/MergingTest.java
@@ -1,5 +1,7 @@
 package org.lxg.basic.sort.merge;
 
+import org.junit.Test;
+
 /**
  * @author: xuegangliu
  * @date: 9/12/2018 10:59 AM
@@ -8,6 +10,7 @@ package org.lxg.basic.sort.merge;
  */
 public class MergingTest {
 
+    @Test
     public void mergTest(){
         int[] a={49,38,65,97,76,13,27,49,78,34,12,64,1,8};
         Merging.printBefore(a);

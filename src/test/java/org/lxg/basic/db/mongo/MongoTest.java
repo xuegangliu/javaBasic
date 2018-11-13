5548304da2f2ae3531fa819747b59dbf5193fdd1
Max <lxghoney@163.com>
2018-11-13 15:20:43 +0800

    mongo数据查询测试

--- src/test/java/org/lxg/basic/db/mongo/MongoTest.java
+++ src/test/java/org/lxg/basic/db/mongo/MongoTest.java
@@ -1,13 +1,17 @@
 package org.lxg.basic.db.mongo;
 
+import com.alibaba.fastjson.JSON;
 import com.mongodb.*;
 import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -27,14 +31,28 @@ public class MongoTest {
         db.drop();
         // 创建表
         List<Document> list = new ArrayList<Document>();
-        Document document1 = new Document("name","tony" );
-        Document document2 = new Document("age",18 );
-        Document document3 = new Document("birthday",new Date());
+        HashMap t = new HashMap(  );
+        t.put( "name","tony" );
+        t.put( "age",18 );
+        t.put( "birthday",new Date());
+        Document document1 = new Document("persn",t);
+        t.put( "age",19 );
+        Document document2 = new Document("persn",t);
+        t.put( "age",20 );
+        Document document3 = new Document("persn",t);
         list.add( document1 );
         list.add( document2 );
         list.add( document3 );
         // 插入集合数据
         db.getCollection( "info" ).insertMany( list );
+
+        MongoCollection collection = db.getCollection( "info" );
+        FindIterable findIterable = collection.find();
+        MongoCursor cursor = findIterable.iterator();
+        while (cursor.hasNext()) {
+            Document doc = (Document) cursor.next();
+            System.out.println(doc.get("_id") + "," + JSON.toJSONString(doc));
+        }
 
     }
 }

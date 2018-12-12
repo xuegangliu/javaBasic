33332121a6cf7eef0364f60c8076df2e0fe28410
guoliang <guoliang@hrtpayment.com>
2018-12-12 17:34:56 +0800

    基于数组的队列结构

--- src/test/java/org/lxg/data/DataStructTest.java
+++ src/test/java/org/lxg/data/DataStructTest.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Random;
 
-public class ArrayTest {
+public class DataStructTest {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -104,6 +105,19 @@ public class ArrayTest {
         logger.info("出队列后:{}",queue);
     }
 
+    @Test
+    public void testLoopQueue(){
+        int opCount = 100000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>(opCount);
+        double time1 = queueT(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>(opCount);
+        double time2 = queueT(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
+    }
+
 
     class Student{
         private String name;
@@ -118,5 +132,17 @@ public class ArrayTest {
         public String toString(){
             return String.format("Student(name: %s, score: %d)", name, score);
         }
+    }
+
+    // 测试使用q运行opCount个enqueueu和dequeue操作所需要的时间，单位：秒
+    public double queueT(BaseQueue<Integer> q, int opCount){
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for(int i = 0 ; i < opCount ; i ++)
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        for(int i = 0 ; i < opCount ; i ++)
+            q.dequeue();
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
     }
 }

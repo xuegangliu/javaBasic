b430ef4ed67e384f0b3ae6fa3b26f653e69dcfd2
guoliang <guoliang@hrtpayment.com>
2019-01-07 16:47:41 +0800

    :new: 线段树结构添加区间方法与修改

--- src/test/java/org/lxg/data/tree/TreeTest.java
+++ src/test/java/org/lxg/data/tree/TreeTest.java
@@ -94,4 +94,15 @@ public class TreeTest {
                 (a, b) -> a + b);
         System.out.println(segTree.toString());
     }
+
+    @Test
+    public void testSegmentTreeQuery(){
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+        SegmentTree<Integer> segTree = new SegmentTree<>(nums,
+                (a, b) -> a + b);
+        System.out.println(segTree);
+        System.out.println(segTree.query(0, 2));
+        System.out.println(segTree.query(2, 5));
+        System.out.println(segTree.query(0, 5));
+    }
 }

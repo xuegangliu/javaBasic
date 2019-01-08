80150c0de1ee36ab38da4a63bb4c9d9de60546b5
guoliang <guoliang@hrtpayment.com>
2019-01-08 14:02:45 +0800

    :new: 添加并查集数据结构-多个版本

--- src/main/java/org/lxg/data/uf/UnionFind1.java
+++ src/main/java/org/lxg/data/uf/UnionFind1.java
@@ -1,14 +1,14 @@
 package org.lxg.data.uf;
 
-/**
- * 并查集UnionFind.java
- */
-public class UnionFind implements UF {
+// 我们的第一版Union-Find
+public class UnionFind1 implements UF {
 
     private int[] id;    // 我们的第一版Union-Find本质就是一个数组
 
-    public UnionFind(int size) {
+    public UnionFind1(int size) {
+
         id = new int[size];
+
         // 初始化, 每一个id[i]指向自己, 没有合并的元素
         for (int i = 0; i < size; i++)
             id[i] = i;
@@ -24,6 +24,7 @@ public class UnionFind implements UF {
     private int find(int p) {
         if(p < 0 || p >= id.length)
             throw new IllegalArgumentException("p is out of bound.");
+
         return id[p];
     }
 
@@ -38,10 +39,13 @@ public class UnionFind implements UF {
     // O(n) 复杂度
     @Override
     public void unionElements(int p, int q) {
+
         int pID = find(p);
         int qID = find(q);
+
         if (pID == qID)
             return;
+
         // 合并过程需要遍历一遍所有元素, 将两个元素的所属集合编号合并
         for (int i = 0; i < id.length; i++)
             if (id[i] == pID)

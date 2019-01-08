397721e75de3c01426f3fc80c44686ce25307787
guoliang <guoliang@hrtpayment.com>
2019-01-08 14:28:09 +0800

    :new: 红黑树数据结构添加

--- src/test/java/org/lxg/data/tree/TreeTest.java
+++ src/test/java/org/lxg/data/tree/TreeTest.java
@@ -109,6 +109,46 @@ public class TreeTest {
     }
 
     @Test
+    public void testBSTFactor(){
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words = new ArrayList<>();
+        if(SetTest.readFile("pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+            BSTFactor<String, Integer> map = new BSTFactor<>();
+            for (String word : words) {
+                if (map.contains(word))
+                    map.set(word, map.get(word) + 1);
+                else
+                    map.add(word, 1);
+            }
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+        }
+        System.out.println();
+    }
+
+    @Test
+    public void testAVLTree(){
+        System.out.println("Pride and Prejudice");
+        ArrayList<String> words = new ArrayList<>();
+        if(SetTest.readFile("pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+            AVLTree<String, Integer> map = new AVLTree<>();
+            for (String word : words) {
+                if (map.contains(word))
+                    map.set(word, map.get(word) + 1);
+                else
+                    map.add(word, 1);
+            }
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+        }
+        System.out.println();
+    }
+
+    @Test
     public void testTrieTime(){
         System.out.println("Pride and Prejudice");
         ArrayList<String> words = new ArrayList<>();

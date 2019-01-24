dd34c57a6f503af18f9c5ed876e39e1914e115ec
guoliang <guoliang@hrtpayment.com>
2019-01-24 16:15:00 +0800

    :add: Add swing gui练习

---
+++ src/main/java/org/lxg/swing/SwingControl.java
@@ -0,0 +1,73 @@
+package org.lxg.swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**************************
+ * @description: SwingControl
+ * @user: xuegangliu
+ * @date: 2019/1/24 16:11
+ ***************************/
+public class SwingControl {
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
+    public SwingControl(){
+        prepareGUI();
+    }
+
+    private void prepareGUI(){
+        mainFrame = new JFrame("Java SWING Examples");
+        mainFrame.setSize(400,400);
+        mainFrame.setLayout(new GridLayout(3, 1));
+        headerLabel = new JLabel("",JLabel.CENTER );
+        statusLabel = new JLabel("",JLabel.CENTER);
+        statusLabel.setSize(350,100);
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+        mainFrame.add(headerLabel);
+        mainFrame.add(controlPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
+    }
+    public void showEventDemo(){
+        headerLabel.setText("Control in action: Button");
+        JButton okButton = new JButton("OK");
+        JButton submitButton = new JButton("Submit");
+        JButton cancelButton = new JButton("Cancel");
+        okButton.setActionCommand("OK");
+        submitButton.setActionCommand("Submit");
+        cancelButton.setActionCommand("Cancel");
+        okButton.addActionListener(new ButtonClickListener());
+        submitButton.addActionListener(new ButtonClickListener());
+        cancelButton.addActionListener(new ButtonClickListener());
+        controlPanel.add(okButton);
+        controlPanel.add(submitButton);
+        controlPanel.add(cancelButton);
+        mainFrame.setVisible(true);
+    }
+    private class ButtonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            if( command.equals( "OK" ))  {
+                statusLabel.setText("Ok Button clicked.");
+            }
+            else if( command.equals( "Submit" ) )  {
+                statusLabel.setText("Submit Button clicked.");
+            }
+            else  {
+                statusLabel.setText("Cancel Button clicked.");
+            }
+        }
+    }
+}
---
+++ src/test/java/org/lxg/swing/SwingTest.java
@@ -0,0 +1,15 @@
+package org.lxg.swing;
+
+
+/**************************
+ * @description: SwingTest
+ * @user: xuegangliu
+ * @date: 2019/1/24 16:12
+ ***************************/
+public class SwingTest {
+
+    public static void main(String[] args) {
+        SwingControl swingControlDemo = new SwingControl();
+        swingControlDemo.showEventDemo();
+    }
+}

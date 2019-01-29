535b902031dbe300fe6fdea61d5fdb12b6bd1c60
hrt-lxg <liuxuegang@hrtpayment.com>
2019-01-29 17:27:58 +0800

    :u: Add 图片压缩测试

--- src/test/java/org/lxg/basic/util/ThumbnailatorTest.java
+++ src/test/java/org/lxg/basic/util/ThumbnailatorTest.java
@@ -19,22 +19,20 @@ import org.junit.Test;
  ***************************/
 public class ThumbnailatorTest {
 
-    // TODO @date:20190129 都未测试
-
     /**
      * 指定大小进行缩放
      *
      * @throws IOException
      */
     @Test
-    private void test1() throws IOException {
+    public void test1() throws IOException {
         /*
          * size(width,height) 若图片横比200小，高比300小，不变
          * 若图片横比200小，高比300大，高缩小到300，图片比例不变 若图片横比200大，高比300小，横缩小到200，图片比例不变
          * 若图片横比200大，高比300大，图片按比例缩小，横为200或高为300
          */
-        Thumbnails.of("images/test.jpg").size(200, 300).toFile("C:/image_200x300.jpg");
-        Thumbnails.of("images/test.jpg").size(2560, 2048).toFile("C:/image_2560x2048.jpg");
+        Thumbnails.of("D:/AA.jpg").size(200, 300).toFile("D:/image_200x300.jpg");
+        Thumbnails.of("D:/AA.jpg").size(2560, 2048).toFile("D:/image_2560x2048.jpg");
     }
 
     /**
@@ -42,12 +40,13 @@ public class ThumbnailatorTest {
      *
      * @throws IOException
      */
-    private void test2() throws IOException {
+    @Test
+    public void test2() throws IOException {
         /**
          * scale(比例)
          */
-        Thumbnails.of("images/test.jpg").scale(0.25f).toFile("C:/image_25%.jpg");
-        Thumbnails.of("images/test.jpg").scale(1.10f).toFile("C:/image_110%.jpg");
+        Thumbnails.of("D:/AA.jpg").scale(0.25f).toFile("D:/image_25%.jpg");
+        Thumbnails.of("D:/AA.jpg").scale(1.10f).toFile("D:/image_110%.jpg");
     }
 
     /**
@@ -55,11 +54,12 @@ public class ThumbnailatorTest {
      *
      * @throws IOException
      */
-    private void test3() throws IOException {
+    @Test
+    public void test3() throws IOException {
         /**
          * keepAspectRatio(false) 默认是按照比例缩放的
          */
-        Thumbnails.of("images/test.jpg").size(120, 120).keepAspectRatio(false).toFile("C:/image_120x120.jpg");
+        Thumbnails.of("D:/AA.jpg").size(120, 120).keepAspectRatio(false).toFile("D:/image_120x120.jpg");
     }
 
     /**
@@ -67,12 +67,13 @@ public class ThumbnailatorTest {
      *
      * @throws IOException
      */
-    private void test4() throws IOException {
+    @Test
+    public void test4() throws IOException {
         /**
          * rotate(角度),正数：顺时针 负数：逆时针
          */
-        Thumbnails.of("images/test.jpg").size(1280, 1024).rotate(90).toFile("C:/image+90.jpg");
-        Thumbnails.of("images/test.jpg").size(1280, 1024).rotate(-90).toFile("C:/iamge-90.jpg");
+        Thumbnails.of("D:/AA.jpg").size(1280, 1024).rotate(90).toFile("D:/image+90.jpg");
+        Thumbnails.of("D:/AA.jpg").size(1280, 1024).rotate(-90).toFile("D:/iamge-90.jpg");
     }
 
     /**
@@ -80,14 +81,15 @@ public class ThumbnailatorTest {
      *
      * @throws IOException
      */
-    private void test5() throws IOException {
+    @Test
+    public void test5() throws IOException {
         /**
          * watermark(位置，水印图，透明度)
          */
-        Thumbnails.of("images/test.jpg").size(1280, 1024).watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File("images/watermark.png")), 0.5f)
-                .outputQuality(0.8f).toFile("C:/image_watermark_bottom_right.jpg");
-        Thumbnails.of("images/test.jpg").size(1280, 1024).watermark(Positions.CENTER, ImageIO.read(new File("images/watermark.png")), 0.5f)
-                .outputQuality(0.8f).toFile("C:/image_watermark_center.jpg");
+        Thumbnails.of("D:/AA.jpg").size(1280, 1024).watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File("D:/0.jpg")), 0.5f)
+                .outputQuality(0.8f).toFile("D:/image_watermark_bottom_right.jpg");
+        Thumbnails.of("D:/AA.jpg").size(1280, 1024).watermark(Positions.CENTER, ImageIO.read(new File("D:/0.jpg")), 0.5f)
+                .outputQuality(0.8f).toFile("D:/image_watermark_center.jpg");
     }
 
     /**
@@ -95,21 +97,22 @@ public class ThumbnailatorTest {
      *
      * @throws IOException
      */
-    private void test6() throws IOException {
+    @Test
+    public void test6() throws IOException {
         /**
          * 图片中心400*400的区域
          */
-        Thumbnails.of("images/test.jpg").sourceRegion(Positions.CENTER, 400, 400).size(200, 200).keepAspectRatio(false)
-                .toFile("C:/image_region_center.jpg");
+        Thumbnails.of("D:/AA.jpg").sourceRegion(Positions.CENTER, 400, 400).size(200, 200).keepAspectRatio(false)
+                .toFile("D:/image_region_center.jpg");
         /**
          * 图片右下400*400的区域
          */
-        Thumbnails.of("images/test.jpg").sourceRegion(Positions.BOTTOM_RIGHT, 400, 400).size(200, 200).keepAspectRatio(false)
-                .toFile("C:/image_region_bootom_right.jpg");
+        Thumbnails.of("D:/AA.jpg").sourceRegion(Positions.BOTTOM_RIGHT, 400, 400).size(200, 200).keepAspectRatio(false)
+                .toFile("D:/image_region_bootom_right.jpg");
         /**
          * 指定坐标
          */
-        Thumbnails.of("images/test.jpg").sourceRegion(600, 500, 400, 400).size(200, 200).keepAspectRatio(false).toFile("C:/image_region_coord.jpg");
+        Thumbnails.of("D:/AA.jpg").sourceRegion(600, 500, 400, 400).size(200, 200).keepAspectRatio(false).toFile("D:/image_region_coord.jpg");
     }
 
     /**
@@ -117,12 +120,13 @@ public class ThumbnailatorTest {
      *
      * @throws IOException
      */
-    private void test7() throws IOException {
+    @Test
+    public void test7() throws IOException {
         /**
          * outputFormat(图像格式)
          */
-        Thumbnails.of("images/test.jpg").size(1280, 1024).outputFormat("png").toFile("C:/image_1280x1024.png");
-        Thumbnails.of("images/test.jpg").size(1280, 1024).outputFormat("gif").toFile("C:/image_1280x1024.gif");
+        Thumbnails.of("D:/AA.jpg").size(1280, 1024).outputFormat("png").toFile("D:/image_1280x1024.png");
+        Thumbnails.of("D:/AA.jpg").size(1280, 1024).outputFormat("gif").toFile("D:/image_1280x1024.gif");
     }
 
     /**
@@ -130,12 +134,13 @@ public class ThumbnailatorTest {
      *
      * @throws IOException
      */
-    private void test8() throws IOException {
+    @Test
+    public void test8() throws IOException {
         /**
          * toOutputStream(流对象)
          */
-        OutputStream os = new FileOutputStream("C:/image_1280x1024_OutputStream.png");
-        Thumbnails.of("images/test.jpg").size(1280, 1024).toOutputStream(os);
+        OutputStream os = new FileOutputStream("D:/image_1280x1024_OutputStream.png");
+        Thumbnails.of("D:/AA.jpg").size(1280, 1024).toOutputStream(os);
     }
 
     /**
@@ -143,11 +148,12 @@ public class ThumbnailatorTest {
      *
      * @throws IOException
      */
-    private void test9() throws IOException {
+    @Test
+    public void test9() throws IOException {
         /**
          * asBufferedImage() 返回BufferedImage
          */
-        BufferedImage thumbnail = Thumbnails.of("images/test.jpg").size(1280, 1024).asBufferedImage();
-        ImageIO.write(thumbnail, "jpg", new File("C:/image_1280x1024_BufferedImage.jpg"));
+        BufferedImage thumbnail = Thumbnails.of("D:/AA.jpg").size(1280, 1024).asBufferedImage();
+        ImageIO.write(thumbnail, "jpg", new File("D:/image_1280x1024_BufferedImage.jpg"));
     }
 }

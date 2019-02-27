42b398a76339648b129dda8a1e7c53d75ae3e12c
hrt-lxg <liuxuegang@hrtpayment.com>
2019-02-27 10:44:27 +0800

    :b: Add Spring utils code

---
+++ core/src/test/java/com/lxg/core/util/CoreUtilsTest.java
@@ -0,0 +1,16 @@
+package com.lxg.core.util;
+
+import org.junit.Test;
+
+/**************************
+ * @description: CoreUtilsTest
+ * @author: xuegangliu
+ * @date: 2019/2/27 10:42
+ ***************************/
+public class CoreUtilsTest {
+
+    @Test
+    public void TestAssert(){
+        Assert.isTrue(false);
+    }
+}
